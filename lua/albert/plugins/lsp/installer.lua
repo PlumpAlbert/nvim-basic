5670b2ada17d75cc5cdd93ebcc2048e76acd6f62
Plump Albert <plumpalbert@gmail.com>
2023-12-26 23:41:06 +0300

    added installer plugin

---
+++ lua/albert/plugins/lsp/installer.lua
@@ -0,0 +1,20 @@
+return {
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	name = "mason-installer",
+	opts = function(_, opts)
+		opts = opts or {}
+		local ensure_installed = vim.list_extend({
+			'lua-language-server',
+			'luacheck',
+			'stylua'
+		}, opts.ensure_installed or {})
+
+		return {
+			ensure_installed = ensure_installed,
+			auto_update = true,
+			run_on_start = true,
+			start_delay = 1000,
+			debounce_hours = 6
+		}
+	end
+}
