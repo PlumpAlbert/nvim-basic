4dd5b347d08c712c3a23dcb94bbe1c7e5d1b1322
Plump Albert <plumpalbert@gmail.com>
2023-12-26 16:00:01 +0300

    added telescope

---
+++ lua/albert/plugins/telescope.lua
@@ -0,0 +1,64 @@
+local M = {
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = "BufEnter",
+	cmd = { "Telescope" },
+}
+
+M.opts = {
+	defaults = {
+		path_display = { "smart" },
+		file_ignore_patterns = { ".git/", "node_modules" },
+	},
+}
+
+M.keys = {
+	{
+		"<leader><space>",
+		function()
+			require("telescope.builtin").find_files(require("telescope.themes").get_dropdown())
+		end,
+		desc = "Find file",
+	},
+	{
+		"<leader>/",
+		function()
+			require("telescope.builtin").live_grep(require("telescope.themes").get_dropdown())
+		end,
+		desc = "Find word",
+	},
+	{
+		"gd",
+		function()
+			require("telescope.builtin").lsp_definitions({
+				jump_type = "vsplit",
+				reuse_win = true,
+			})
+		end,
+		desc = "Go to declaration",
+	},
+	{
+		"gI",
+		function()
+			require("telescope.builtin").lsp_implementations({
+				jump_type = "vsplit",
+				reuse_win = true,
+			})
+		end,
+		desc = "Go to implementation",
+	},
+	{
+		"gr",
+		function()
+			require("telescope.builtin").lsp_references({
+				jump_type = "vsplit",
+			})
+		end,
+		desc = "Go to references",
+	},
+}
+
+return M
