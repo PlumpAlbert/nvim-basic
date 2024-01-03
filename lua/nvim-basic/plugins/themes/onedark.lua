99b9bc6c86f771347f9528f9322c97fab7e289e4
Plump Albert <plumpalbert@gmail.com>
2024-01-03 09:31:00 +0300

    use onedark theme by default

--- lua/nvim-basic/plugins/themes/onedark.lua
+++ lua/nvim-basic/plugins/themes/onedark.lua
@@ -1,26 +1,29 @@
 return {
 	"olimorris/onedarkpro.nvim",
 	priority = 99,
-	lazy = true,
-	opts = {
-		types = "italic",
-		keywords = "italic",
-		virtual_text = "italic",
-		plugins = {
-			gitsigns = true,
-			lsp_semantic_tokens = true,
-			nvim_cmp = true,
-			nvim_lsp = true,
-			nvim_ts_rainbow = true,
-			telescope = true,
-			treesitter = true,
-			which_key = true,
-		},
-		options = {
-			cursorline = false,
-			transparency = true,
-			terminal_colors = true,
-			highlight_inactive_windows = false,
-		},
-	},
+	lazy = false,
+	config = function()
+		require("onedarkpro").setup({
+			types = "italic",
+			keywords = "italic",
+			virtual_text = "italic",
+			plugins = {
+				gitsigns = true,
+				lsp_semantic_tokens = true,
+				nvim_cmp = true,
+				nvim_lsp = true,
+				nvim_ts_rainbow = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
+			},
+			options = {
+				cursorline = false,
+				transparency = true,
+				terminal_colors = true,
+				highlight_inactive_windows = false,
+			},
+		})
+		vim.cmd.colorscheme("onedark_dark")
+	end,
 }
